13e2d12040bcd5a25188fa5e3d85788f7e5721c3
Polinky9999 <120062425+Polinky9999@users.noreply.github.com>
2023-05-07 18:58:16 +0900

    app.py

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("文章要約")
+st.write("ChatGPT APIを使った文章要約を行います。")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input("要約したい文章を入力してください。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
